53ae6e7baa59d6ebd65acb8d1a688f10e33d8e96
AbdelrahmanEldoma <113690967+AbdelrahmanEldoma@users.noreply.github.com>
2023-06-05 21:52:12 +0300

    Homework

---
+++ Homework 1/script.js
@@ -0,0 +1,14 @@
+function RunJS() {
+  alert("JS is running");
+}
+
+function Phone() {
+    window.location.href = "tel:+1 202-918-2132";
+  }
+  
+  function Email() {
+    window.location.href = "mailto:random@gmail.com";
+  }
+  
+
+  
